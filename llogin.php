975d4dcc3bd0ccf441be72b9708b4b93b549ab18
laith94 <59922364+laith94@users.noreply.github.com>
2020-02-02 10:56:27 +0300

    h

---
+++ llogin.php
@@ -0,0 +1,15 @@
+<?php
+
+  include "header.html";
+
+?>
+<table>
+    <th>الرقم</th>
+    <th>اسم المستخدم</th>
+    <th>اسم العائلة</th>
+    <th>الايميل</th>
+    <th>الايميل</th>
+    <th>كلمة السر</th>
+    <th>المواليد</th>
+    
+</table>
